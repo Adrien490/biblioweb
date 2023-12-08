6a4afe05e158bd7c952b1c3d6c837b68f69f4b70
oui-connect-adrien <adrien.poirier@oui-connect.fr>
2023-12-08 15:51:33 +0100

    Update page.tsx

--- app/(users)/(routes)/page.tsx
+++ app/(users)/(routes)/page.tsx
@@ -27,7 +27,7 @@ const HomePage = () => {
       </section>
 
       {/* Section Formules */}
-      <section className="bg-gray-100 py-12">
+      <section className="bg-muted py-12">
         <div className="container mx-auto px-6 md:px-12">
           <h2 className="text-4xl font-bold text-center mb-8">Nos Formules</h2>
           <div className="flex flex-wrap justify-center">
@@ -36,18 +36,6 @@ const HomePage = () => {
         </div>
       </section>
 
-      {/* Section Événements */}
-      <section className="container mx-auto px-6 md:px-12 my-12">
-        <h2 className="text-4xl font-bold text-center mb-8">Événements à Venir</h2>
-        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6">
-          {/* Ici, ajoutez des composants de carte pour les événements à venir */}
-        </div>
-      </section>
-
-      {/* Pied de page */}
-      <footer className="bg-gray-800 text-white text-center py-8">
-        <p>© 2023 Médiathèque. Tous droits réservés.</p>
-      </footer>
     </div>
   );
 };
